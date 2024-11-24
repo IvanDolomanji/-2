31642628d4c3be253b0638911bc0c884ec202881
IvanDolomanji <156667166+IvanDolomanji@users.noreply.github.com>
2024-11-24 21:10:29 +0200

    Add files via upload

---
+++ пр2/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace пр2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1
+            //Console.Write("Введите первое число: ");
+            //int a = int.Parse(Console.ReadLine());
+            //Console.Write("Введите второе число: ");
+            //int b = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine($"Сумма: {a + b}");
+            //Console.WriteLine($"Произведение: {a * b}");
+
+            //2
+            //Console.Write("Введите первое число: ");
+            //int a = int.Parse(Console.ReadLine());
+            //Console.Write("Введите второе число: ");
+            //int b = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine($"Прямой порядок: {a}, {b}");
+            //Console.WriteLine($"Обратный порядок: {b}, {a}");
+            //Console.WriteLine($"Сумма: {a + b}");
+            //Console.WriteLine($"Произведение: {a * b}");
+
+            //3
+            //Console.Write("Введите первое число: ");
+            //int a = int.Parse(Console.ReadLine());
+            //Console.Write("Введите второе число: ");
+            //int b = int.Parse(Console.ReadLine());
+            //Console.Write("Введите третье число: ");
+            //int c = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine($"Сумма: {a + b + c}");
+
+            //4
+            //Console.Write("Введите первое число: ");
+            //double a = double.Parse(Console.ReadLine());
+            //Console.Write("Введите второе число: ");
+            //double b = double.Parse(Console.ReadLine());
+
+            //Console.WriteLine($"Произведение: {Math.Round(a * b, 1)}");
+
+            //5
+            //Console.Write("Введите первое число: ");
+            //double a = double.Parse(Console.ReadLine());
+            //Console.Write("Введите второе число: ");
+            //double b = double.Parse(Console.ReadLine());
+            //if (b != 0)
+            //{
+            //    Console.WriteLine($"Результат деления: {Math.Round(a / b, 3)}");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Ошибка: деление на ноль!");
+            //}
+
+            //6
+            //Console.Write("Введите вещественное число: ");
+            //double a = double.Parse(Console.ReadLine());
+
+            //Console.WriteLine($"Вы ввели число: {Math.Round(a, 2)}");
+
+            //7
+            //Console.Write("Введите первое число: ");
+            //int a = int.Parse(Console.ReadLine());
+            //Console.Write("Введите второе число: ");
+            //int b = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine($"Сумма: {a + b}");
+            //Console.WriteLine($"Разность: {a - b}");
+            //Console.WriteLine($"Произведение: {a * b}");
+
+            //8
+            //Console.WriteLine("Введите текст (для завершения нажмите Enter): ");
+            //string input = Console.ReadLine();
+
+            //Console.WriteLine($"Вы ввели {input.Length} символов.");
+            //Console.ReadKey();
+
+            //часть 2
+            //1
+            //Console.WriteLine("Введите радиус окружности:");
+            //double r = double.Parse(Console.ReadLine());
+            //Console.WriteLine($"Длина: {2 * Math.PI * r}, Площадь: {Math.PI * r * r}");
+
+            //2
+            //Console.WriteLine("Количество жителей и площадь:");
+            //Console.WriteLine($"Плотность: {double.Parse(Console.ReadLine()) / double.Parse(Console.ReadLine())}");
+
+            //3
+            //Console.WriteLine("Катеты прямоугольного треугольника:");
+            //double p = double.Parse(Console.ReadLine()), q = double.Parse(Console.ReadLine());
+            //Console.WriteLine($"Периметр: {p + q + Math.Sqrt(p * p + q * q)}");
+
+            //4
+            //Console.WriteLine("Стороны прямоугольника:");
+            //double a = double.Parse(Console.ReadLine()), b = double.Parse(Console.ReadLine());
+            //Console.WriteLine($"Периметр: {2 * (a + b)}, Диагональ: {Math.Sqrt(a * a + b * b)}");
+
+            //5
+            //Console.WriteLine("Координаты двух точек:");
+            //var coords = Console.ReadLine().Split();
+            //double x1 = double.Parse(coords[0]), y1 = double.Parse(coords[1]);
+            //double x2 = double.Parse(coords[2]), y2 = double.Parse(coords[3]);
+            //Console.WriteLine($"Расстояние: {Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1))}");
+
+            //7
+            //Console.WriteLine("Двузначное число:");
+            //int n = int.Parse(Console.ReadLine());
+            //Console.WriteLine($"Десятки: {n / 10}, Единицы: {n % 10}, Сумма: {n / 10 + n % 10}, Произведение: {(n / 10) * (n % 10)}");
+            Console.Read();
+
+
+        }
+    }
+}
